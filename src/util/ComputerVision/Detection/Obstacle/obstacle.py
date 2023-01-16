caaf09d747639ddd0f74c88846b08e198b7f4b9d
williams181 <william.roberto39@hotmail.com>
2023-01-16 02:21:31 -0300

    last?

--- src/util/ComputerVision/Detection/Obstacle/obstacle.py
+++ src/util/ComputerVision/Detection/Obstacle/obstacle.py
@@ -1,47 +1,61 @@
-    # scan top to bottom, left to right and compute the 'width' of each black stripe.
-# if its too narrow - being < 0.45 times the row number then turn it to white - thus cancelling it as a navigable area
-# the factor was computed in excel and represents a trapezium - narrow at the top (far away), wide at the bottom (up close)
-# (1st plan) the furthest acceptable area is our target - we need, eventually, a bearing to stear
-import cv2
+import cv2 as cv
+import numpy as np
 
-factor = 0.45
-targetArea = [maxRow, minCol, maxCol]  # thats row, start-col, end-col
-for row in range((minRow + 2), (maxRow - 2), 1):      
-    minWidthForRow = max(int(row*factor), 40) # dont accept stupid widths - in infinite distance its the eye of a needle
-    inClearSpace = 0
-    widthCount = 0
-    startCol = minCol
-    endCol = maxCol
-    for col in range (minCol, maxCol, 1):  #for the width of image array
-        if inClearSpace == 0 :
-            if NavigableArea[row, col] == Grayscale_black:   #check to see if the pixel is black(0) which indicates start of clear-space
-                inClearSpace = 1                             # turn counting mode on
-                startCol = col                               # remember where it started
-        else:                                                # in inClearSpace 
-            if NavigableArea[row, col] != Grayscale_black:   # if the next pixel has gone to white we are the end of the clearspace
-                endCol = col                                 #so see if it was wide enough
-                if (endCol - startCol) <= minWidthForRow :   # too narrow so zap it 
-                    NavigableArea[row, startCol:endCol] = Grayscale_white
-                else:                                       # else: leave it alone its valid, see if its our best guess yet
-                    if row < targetArea [0] :
-                        targetArea [0] = row
-                        targetArea [1] = startCol
-                        targetArea [2] = endCol
-                    elif row == targetArea [0] and ((endCol - startCol) > (targetArea [2] - targetArea [1] )) : # same row but wider target
-                        targetArea [1] = startCol
-                        targetArea [2] = endCol
-                inClearSpace = 0                             # regardless, say we are out of ClearSpace
-# If we found something draw a line from middle of target to 'base' to prove which way we are going
-if targetArea [0] != maxRow:  # not its start value
-    # I think lines use x,y - backwards from the row,col we have been using ie x=col and y=row
-    NavigableAreaColor = cv2.cvtColor(NavigableArea,cv2.COLOR_GRAY2BGR)
-    fromx = int(targetArea[1] + ((targetArea[2] - targetArea[1]) / 2))  # start col + half width
-    fromy = int(targetArea [0])
-    tox = int(maxCol / 2)        # middle of the array
-    toy = int(maxRow)            # last line
-    cv2.line(NavigableAreaColor, (fromx, fromy), (tox, toy), (0,0,255),2)
-    #                              From           to        colour   thichness
-# show result
-tempNavigableArea2 = NavigableArea.copy()
-cv2.imshow('Win 4-4', tempNavigableArea2)
-cv2.imshow('Win 4-5', NavigableAreaColor)
+cap = cv.VideoCapture("data\\videos\\input\\ifpe (30).mp4")
+
+while True:
+    _, frame1 = cap.read()
+    frame = cv.blur(frame1, (20, 20))
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([180, 255, 40])
+
+    mask = cv.inRange(hsv, lower_black, upper_black)
+    res = cv.bitwise_not(mask)
+    # cv.imshow('mask',mask)
+
+    contours, _ = cv.findContours(res, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cv.drawContours(frame, contours, 0, (0, 255, 0), 3)
+
+    # cv.imshow('res',res)
+
+    coordinates = (250, 100)
+    font = cv.FONT_HERSHEY_SIMPLEX
+    fontScale = 1
+    color = (0, 0, 255)
+    thickness = 2
+
+    try:
+        cnt = contours[0]
+
+        M = cv.moments(cnt)
+        try:
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+
+        except ZeroDivisionError:
+            continue
+
+        if cx < 320:
+            text = "A esquerda..."
+            # Send signal to motor driver to turn left
+        else:
+            text = "A direita..."
+            # Send signal to motor driver to turn right
+
+    except IndexError:
+        continue
+    frame = cv.putText(frame, text, coordinates, font,
+                       fontScale, color, thickness, cv.LINE_AA)
+
+    
+    imS = cv.resize(frame, (960, 540)) 
+    cv.imshow("img", imS)
+
+    k = cv.waitKey(15) & 0xFF  # Esc button to quit
+    if k == ord('q') or k == 27:
+        break
+
+cv.destroyAllWindows()
+# cap.release()
